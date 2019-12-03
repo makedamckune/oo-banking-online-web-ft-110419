590973407e1165eeafc248d30c3c964a9407cea5
Makeda P Mckune <makedamckune@gmail.com>
2019-12-03 00:42:56 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
+  attr_accessor :balance, :status 
+  attr_reader :name 
+ 
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+    @status = "open"
+  end 
+  
+  def deposit(amount)
+    @balance += amount
+  end 
+  
+  def display_balance
+    puts "Your balance is $#{@balance}"
+  end
+  
+  
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+ end 
 
 end
