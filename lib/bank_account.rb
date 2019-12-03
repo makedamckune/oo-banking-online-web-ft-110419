17d4a5a94dadf58da0e9a486a815cf863b065bc1
Makeda P Mckune <makedamckune@gmail.com>
2019-12-03 00:09:10 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,12 @@
 class BankAccount
+  attr_accessor :balance, :status 
+  attr_reader :name 
+ 
+  def initialize(avi)
+     
+    @name = name 
+    @balance = 1000
+    @status = "Open"
+  end 
 
 end
